d3d4b3bc225466d96f987841412f9652a65f4672
Walther Oliveira <waltherpires@hotmail.com>
2024-08-31 00:59:49 -0300

    Implementando a logica de xequemate

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -41,6 +41,9 @@ namespace xadrez_console
                         Console.ReadLine();
                     }
                 }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
+
 
             }
             catch (TabuleiroException e)
